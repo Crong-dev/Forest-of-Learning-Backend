dddd729a40035c8aa025aad76f4b892c2ab37059
sebikawa32 <tpqlsdl150@naver.com>
2026-04-15 09:46:47 +0900

    feat: 디테일 관련 api 로직 구현

--- src/routes/emoji.routes.js
+++ src/routes/emoji.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createEmojiReaction, getEmojiReactions } from '../controllers/emoji.controller.js';
+import {
+  addEmojiReaction,
+  getEmojiReactions,
+} from '../controllers/emoji.controller.js';
 
 const router = express.Router();
 
-router.post('/', createEmojiReaction);
+router.post('/', addEmojiReaction);
 router.get('/', getEmojiReactions);
 
 export default router;
