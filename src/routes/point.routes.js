9bb86d42de6b051da0de82128e4299fdfcc2aa77
Sim robin <rnepsxkr199@gmail.com>
2026-04-20 14:11:06 +0900

    feat: 포인트로그 추가/조회 기능 추가

--- src/routes/point.routes.js
+++ src/routes/point.routes.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getPoint, addPoints } from '../controllers/point.controller.js';
+import {
+  getPoint,
+  addPoints,
+  getPointLogs,
+} from '../controllers/point.controller.js';
 import { numericParams } from '../middlewares/validateParams.js';
 
 const router = express.Router();
 
 router.get('/:studyId', numericParams('studyId'), getPoint);
-router.patch('/:studyId', numericParams('studyId', 'amount'), addPoints);
+router.get('/:studyId/logs', numericParams('studyId'), getPointLogs);
+router.patch('/:studyId', numericParams('studyId'), addPoints);
 
 export default router;
